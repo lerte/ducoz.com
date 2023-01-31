8d61421e196499f2a51765e2d8cf0fbe5a8540e3
lerte smith <lerte@qq.com>
2023-01-31 19:04:36 +0800

    update header

--- components/Header.vue
+++ components/Header.vue
@@ -24,7 +24,7 @@
 
       <div
         :class="navContentClassList"
-        class="w-full flex-grow lg:flex lg:items-center lg:w-auto mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
+        class="w-full flex-grow lg:flex lg:items-center lg:w-auto mt-2 lg:mt-0 lg:bg-transparent text-black p-4 lg:p-0 z-20"
       >
         <ul class="list-reset lg:flex justify-end flex-1 items-center">
           <li class="mr-3" v-for="(nav, index) in menu" :key="index">
