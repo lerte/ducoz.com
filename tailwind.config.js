2bca990dad495616fb0deec988da0a2d3ada6eba
Lerte Smith <lertesmith@gmail.com>
2023-02-01 00:19:05 +0800

    migrate tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
-const colors = require('tailwindcss/colors')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./layouts/**/*.vue', './components/**/*.vue', './pages/**/*.vue'],
+  content: [],
   theme: {
-    colors: {
-      ...colors,
-      primary: '#102B6A', // 主色调
-      secondary: '#426AB3', // 次颜色
-      success: '#1D953F',
-      danger: '#ED1941',
-      warning: '#FFC20E',
-      info: '#4e72b8',
-      light: '#999D9C',
-      dark: '#130c0e'
+    extend: {
+      colors: {
+        primary: '#102B6A', // 主色调
+        secondary: '#426AB3', // 次颜色
+        success: '#1D953F',
+        danger: '#ED1941',
+        warning: '#FFC20E',
+        info: '#4e72b8',
+        light: '#999D9C',
+        dark: '#130c0e'
+      }
     }
   },
-  plugins: [require('@tailwindcss/typography')]
+  plugins: []
 }
