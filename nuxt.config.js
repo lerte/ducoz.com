43e208c30819209eb3eec7f54e66da295eb4d944
Lerte Smith <lertesmith@gmail.com>
2023-02-01 00:50:41 +0800

    atlogic

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
+  serverMiddleware: [{ path: '/api', handler: '~/api/index.js' }],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
