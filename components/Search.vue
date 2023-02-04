5e9d8cc656413d6683acc961a7dc81b5da790fcb
lerte smith <lerte@qq.com>
2023-02-04 19:45:58 +0800

    错误页面

---
+++ components/Search.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <v-row
+      dense
+      class="ml-2"
+      :key="index"
+      v-for="(items, index) in searchItems"
+    >
+      <v-col :key="searchItem.value" v-for="searchItem in items">
+        <v-autocomplete
+          v-if="dicts[searchItem.value]"
+          dense
+          outlined
+          clearable
+          hide-details
+          :label="searchItem.text"
+          :items="dicts[searchItem.value]"
+          v-model="search[searchItem.value]"
+        />
+        <v-text-field
+          v-else
+          dense
+          outlined
+          clearable
+          hide-details
+          :label="searchItem.text"
+          @keyup.enter.native="doSearch"
+          v-model="search[searchItem.value]"
+        />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import chunk from 'lodash/chunk'
+import debounce from 'lodash/debounce'
+export default {
+  name: 'search',
+  props: {
+    dicts: {
+      type: Object,
+      default: () => ({})
+    },
+    headers: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data: () => ({
+    search: {}
+  }),
+  watch: {
+    search: {
+      handler() {
+        this.doSearch()
+      },
+      deep: true
+    }
+  },
+  computed: {
+    searchItems() {
+      const items = this.headers.filter((i) => i.searchable == true)
+      if (items.length <= 10) {
+        return chunk(items, 10)
+      } else {
+        return chunk(items, Math.ceil(items.length / 2))
+      }
+    }
+  },
+  created() {
+    const items = this.headers.filter((i) => i.default)
+    items.forEach((item) => {
+      this.$set(this.search, item.value, item.default)
+    })
+
+    const keys = Object.keys(this.$route.params)
+    keys.forEach((key) => {
+      this.$set(this.search, key, this.$route.params[key])
+    })
+  },
+  methods: {
+    removeEmpty(obj) {
+      Object.keys(obj).forEach((k) => {
+        if (obj[k] == null) {
+          delete obj[k]
+        }
+        if (typeof obj[k] == 'string') {
+          obj[k] = obj[k].trim()
+        }
+        if (obj[k] == '') {
+          delete obj[k]
+        }
+        if (!obj[k] && obj[k] != 0) {
+          delete obj[k]
+        }
+      })
+      return obj
+    },
+    doSearch: debounce(function () {
+      this.$emit('doSearch', this.removeEmpty(this.search))
+    }, 800),
+    setSearch({ key, value }) {
+      this.$set(this.search, key, value)
+    }
+  }
+}
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="flex flex-col items-center justify-center py-16 h-96 gap-4 px-4">
+    <h1 v-if="error.statusCode === 404" class="text-white">页面没找到</h1>
+    <h1 v-else class="text-white">服务器出错</h1>
+    <NuxtLink to="/" class="text-white font-bold text-lg md:mb-6">
+      返回首页
+    </NuxtLink>
+  </main>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'error' // you can set a custom layout for the error page
+}
+</script>
