fa52fb60aa77fc89d8111fc5dcac9ec6c8ea5562
lerte smith <lerte@qq.com>
2023-02-07 19:06:37 +0800

    前台业务

---
+++ pages/business/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main
+    class="h-full py-16 px-4 space-y-4 flex flex-col text-center items-center justify-center"
+  >
+    <p>更多功能正在开发中...</p>
+    <v-btn color="primary" to="/">
+      <v-icon left>mdi-home</v-icon>返回首页
+    </v-btn>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
