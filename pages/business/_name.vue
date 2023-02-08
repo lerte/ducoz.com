07a2dc2a1b587863a321cbf8e74d0b190ce8cc85
lerte smith <lerte@qq.com>
2023-02-08 12:52:11 +0800

    更新业务介绍页面模板

--- pages/business/_name.vue
+++ pages/business/_name.vue
@@ -1,28 +1,42 @@
 <template>
   <main
-    class="h-[calc(100vh_-_472px)] flex flex-col text-center items-center justify-center bg-gray-100"
+    class="h-min-[calc(100vh_-_472px)] flex flex-col text-center items-center justify-center bg-gray-100"
   >
-    <article v-if="business.length" v-html="business[0].content"></article>
-    <article v-else>暂无内容！</article>
+    <div class="container mt-16">
+      <article
+        class="text-left tiptap-vuetify-editor__content"
+        v-if="business.length"
+        v-html="business[0]?.content"
+      />
+      <NuxtLink
+        v-if="business.length"
+        :to="`/user/${business[0]?.name}`"
+        class="px-4 py-2 font-semibold bg-secondary rounded-sm text-white shadow-sm mr-3"
+      >
+        马上进入
+      </NuxtLink>
+    </div>
   </main>
 </template>
+
+<style scoped>
+table {
+  border: 1px solid;
+}
+</style>
 
 <script>
 export default {
   name: 'business',
-  async asyncData({ app, redirect, params }) {
-    if (params.name == 'checkmail') {
-      redirect(302, '/user/checkmail')
+  async asyncData({ app, params }) {
+    const { data, errors } = await app.$altogic.db
+      .model('businesses')
+      .filter(`name == "${params.name}"`)
+      .get()
+    if (!errors) {
+      return { business: data }
     } else {
-      const { data, errors } = await app.$altogic.db
-        .model('businesses')
-        .filter(`name == "${params.name}"`)
-        .get()
-      if (!errors) {
-        return { business: data }
-      } else {
-        return { business: [] }
-      }
+      return { business: [] }
     }
   }
 }
