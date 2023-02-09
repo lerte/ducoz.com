0ae5eab6e74ea7a93d5369f991ab82de358f29bd
lerte smith <lerte@qq.com>
2023-02-09 18:27:11 +0800

    广告开户

---
+++ pages/user/google-ad-account.vue
@@ -0,0 +1,13 @@
+<template>
+  <main
+    class="h-full py-16 px-4 space-y-4 flex flex-col text-center items-center justify-center"
+  >
+    <p>开发中...</p>
+  </main>
+</template>
+<script>
+export default {
+  name: 'google-ad-account',
+  layout: 'user'
+}
+</script>
