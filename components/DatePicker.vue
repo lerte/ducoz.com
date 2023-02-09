51a7b98e2c840d420135c973f5f6506baac7091c
lerte smith <lerte@qq.com>
2023-02-09 21:35:14 +0800

    日期选择器

---
+++ components/DatePicker.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-text-field
+    dense
+    outlined
+    readonly
+    hide-details
+    :label="label"
+    :value="date"
+    :rules="rules"
+    @click="show = true"
+  >
+    <template #append>
+      <v-menu
+        top
+        v-model="show"
+        nudge-left="290"
+        nudge-bottom="10"
+        transition="scale-transition"
+        :close-on-content-click="true"
+      >
+        <template #activator="{ on }">
+          <v-icon v-on="on">mdi-calendar</v-icon>
+        </template>
+        <v-date-picker
+          no-title
+          :value="date"
+          @change="change"
+          locale="zh-cn"
+          :min="min"
+        />
+      </v-menu>
+    </template>
+  </v-text-field>
+</template>
+
+<script>
+export default {
+  name: 'date-picker',
+  props: ['label', 'date', 'min', 'max', 'rules'],
+  model: {
+    prop: 'date',
+    event: 'change'
+  },
+  data: () => ({
+    show: false
+  }),
+  methods: {
+    change(event) {
+      this.$emit('change', event)
+    }
+  }
+}
+</script>
